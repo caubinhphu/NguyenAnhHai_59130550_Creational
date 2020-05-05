6175fc5d4b7b816c693c0241df7a7d8859cd0b38
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-05-05 12:47:03 +0700

    init

---
+++ src/nguyenanhhai_59130550_creational/NguyenAnhHai_59130550_Creational.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nguyenanhhai_59130550_creational;
+
+/**
+ *
+ * @author Hai Tran Do
+ */
+public class NguyenAnhHai_59130550_Creational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
